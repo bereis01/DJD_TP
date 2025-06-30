aed47f97d8c5db4d722488b9554e9d469d3fdbab
Bernardo Reis <bernardo.reis.almeida01@gmail.com>
2025-06-30 13:17:08 -0300

    Removed obstacles from map

--- Assets/Levels/Wall_tiles.tsx
+++ Assets/Levels/Wall_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Wall_tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
- <image source="../../Downloads/Pixel Crawler - Free Pack 2.0.4/Pixel Crawler - Free Pack/Environment/Tilesets/Wall_Tiles.png" width="400" height="400"/>
+ <image source="Source/Pixel Crawler - Free Pack/Environment/Tilesets/Wall_Tiles.png" width="400" height="400"/>
 </tileset>
